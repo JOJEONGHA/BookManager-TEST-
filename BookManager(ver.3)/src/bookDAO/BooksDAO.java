163bae5d134775995a8ca34fae5a9edb6c599702
MongEe <wosd1233@gmail.com>
2019-08-28 20:37:42 +0900

    도서 삭제 완료(복수개 작가 적용)

--- BookManager(ver.3)/src/bookDAO/BooksDAO.java
+++ BookManager(ver.3)/src/bookDAO/BooksDAO.java
@@ -19,9 +19,9 @@ import bookVO.JoinTable;
 import bookVO.Pieces;
 
 public class BooksDAO {
-    private Connection con;
+	private Connection con;
 	private PreparedStatement pstmt;
-    private DataSource dataFactory;
+	private DataSource dataFactory;
 
 	public BooksDAO() {
 		try {
@@ -31,109 +31,106 @@ public class BooksDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
-	
+	}
+
 	// (info_UD - 선택한 책 정보)
-    public JoinTable bookInfo(String num) {
-    	JoinTable info = new JoinTable();
-    	
-    	List<JoinTable> list = new ArrayList<JoinTable>();
-    	list = listBooks();
-    	// 글쓴이 수정하는 거는 복수개가 들어가므로 신경써야한다.
-    	for(JoinTable i : list) {
-    		String temp = i.getBooknum();
-    		String temp_ = num;
-    		System.out.println(temp.equals(temp_));
-    		if(temp.equals(num)) {
-    			info = i;	// TROBLE : 안들어가니꺼 Null인 거지
-    			break;
-    		}
-    	}
-    	return info;
-    }
-	
-    // (Main - 책 리스트)
-    public List<JoinTable> listBooks(){
-        List<JoinTable> list = new ArrayList<JoinTable>();
-        try {
-            con = dataFactory.getConnection();
-            String query = "SELECT * "
-					+ "FROM books b "
-					+ "INNER JOIN pieces p ON b.booknum = p.booknum "
-                    + "inner join authors a on p.authornum = a.authornum "
-                    + "order by b.title desc";
-            pstmt = con.prepareStatement(query);
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
+	public JoinTable bookInfo(String num) {
+		JoinTable info = new JoinTable();
+
+		List<JoinTable> list = new ArrayList<JoinTable>();
+		list = listBooks();
+		// 글쓴이 수정하는 거는 복수개가 들어가므로 신경써야한다.
+		for (JoinTable i : list) {
+			String temp = i.getBooknum();
+			String temp_ = num;
+			System.out.println(temp.equals(temp_));
+			if (temp.equals(num)) {
+				info = i; // TROBLE : 안들어가니꺼 Null인 거지
+				break;
+			}
+		}
+		return info;
+	}
+
+	// (Main - 책 리스트)
+	public List<JoinTable> listBooks() {
+		List<JoinTable> list = new ArrayList<JoinTable>();
+		try {
+			con = dataFactory.getConnection();
+			String query = "SELECT * " + "FROM books b " + "INNER JOIN pieces p ON b.booknum = p.booknum "
+					+ "inner join authors a on p.authornum = a.authornum " + "order by b.title desc";
+			pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
 				String booknum = rs.getString("booknum");
-                String title = rs.getString("title");
-                String authorname = rs.getString("authorname");
-                String publisher = rs.getString("publisher");
-                String summary = rs.getString("summary");
-                String authornum = rs.getString("authornum");
-                String birthyear = rs.getString("birthyear");
-                JoinTable jt = new JoinTable();
-                
-                // Same authors catch
-                while(true){
-                    boolean switch_ = rs.next();
-                    
-                    if(switch_ == true) {
-                    	String temp = rs.getString("booknum");
-                    	if(booknum.equals(temp))
-                            authorname += ", " + rs.getString("authorname");
-                        else{
-                            rs.previous();
-                            System.out.println("test");
-                            break;
-                        }
-                    }else {       	
-                    	System.out.println("test");
-                    	break;
-                    }
-                    
-                }
-                System.out.println("test");
-                jt.setBooknum(booknum);
-                jt.setTitle(title);
-                jt.setAuthorname(authorname);
-                jt.setPublisher(publisher);
-                jt.setSummary(summary);
-                jt.setAuthornum(authornum);
-                jt.setBirthyear(birthyear);
+				String title = rs.getString("title");
+				String authorname = rs.getString("authorname");
+				String publisher = rs.getString("publisher");
+				String summary = rs.getString("summary");
+				String authornum = rs.getString("authornum");
+				String birthyear = rs.getString("birthyear");
+				JoinTable jt = new JoinTable();
+
+				// Same authors catch
+				while (true) {
+					boolean switch_ = rs.next();
+
+					if (switch_ == true) {
+						String temp = rs.getString("booknum");
+						if (booknum.equals(temp)) {
+							authorname += ", " + rs.getString("authorname");
+							authornum += ", " + rs.getString("authornum");
+						} else {
+							rs.previous();
+							System.out.println("test");
+							break;
+						}
+					} else {
+						System.out.println("test");
+						break;
+					}
+
+				}
+				System.out.println("test");
+				jt.setBooknum(booknum);
+				jt.setTitle(title);
+				jt.setAuthorname(authorname);
+				jt.setPublisher(publisher);
+				jt.setSummary(summary);
+				jt.setAuthornum(authornum);
+				jt.setBirthyear(birthyear);
 				list.add(jt);
 			}
 			rs.close();
 			pstmt.close();
 			con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-        return list;
-    }
-    
-    // (create_C - 책 추가 시 일련번호 생성)
-    public String firstCreate() {
-    	Date today = new Date();
-        SimpleDateFormat date = new SimpleDateFormat("yyMMddhhmmss");
+		return list;
+	}
+
+	// (create_C - 책 추가 시 일련번호 생성)
+	public String firstCreate() {
+		Date today = new Date();
+		SimpleDateFormat date = new SimpleDateFormat("yyMMddhhmmss");
 		String now = date.format(today);
 		// book code is start 01-
-        String booknum = "01-" + now;
-        
-        return booknum;
-    }
-	
+		String booknum = "01-" + now;
+
+		return booknum;
+	}
+
 	// (Create_C - 해당 정보의 작가 번호 return)
-	public String whatAuthor(String name, String birth){
+	public String whatAuthor(String name, String birth) {
 		String num = new String();
 		try {
 			con = dataFactory.getConnection();
-			String query = "SELECT * FROM authors" 
-							+ " WHERE authorname = '" + name
-							+ "' and birthyear = '" + birth + "'";
+			String query = "SELECT * FROM authors" + " WHERE authorname = '" + name + "' and birthyear = '" + birth
+					+ "'";
 			pstmt = con.prepareStatement(query);
 			ResultSet rs = pstmt.executeQuery();
-			if(rs.next() == false)
+			if (rs.next() == false)
 				return null;
 			num = rs.getString("authornum");
 		} catch (SQLException e) {
@@ -143,27 +140,26 @@ public class BooksDAO {
 		return num;
 	}
 
-    // (enroll_C - 같은 이름,생일 가진 작가 유무 판단 후 없으면 db에 추가)
-    public boolean checkAuthor(Authors a) {
-    	String temp_name = a.getAuthorname();
-    	String temp_birth = a.getBirthyear();
-    	String query = "SELECT * FROM authors WHERE authorname= '" 
-    					+ temp_name + "' and birthyear= '" 
-    					+ temp_birth + "'";
-    	
-    	try {
-    		con = dataFactory.getConnection();
+	// (enroll_C - 같은 이름,생일 가진 작가 유무 판단 후 없으면 db에 추가)
+	public boolean checkAuthor(Authors a) {
+		String temp_name = a.getAuthorname();
+		String temp_birth = a.getBirthyear();
+		String query = "SELECT * FROM authors WHERE authorname= '" + temp_name + "' and birthyear= '" + temp_birth
+				+ "'";
+
+		try {
+			con = dataFactory.getConnection();
 			pstmt = con.prepareStatement(query);
 			ResultSet rs = pstmt.executeQuery();
-			if(rs.next() == true) 
+			if (rs.next() == true)
 				return false;
-			
+
 			// Authors Code Setting
 			// Authors code is start 00-
 			Date today = new Date();
 			SimpleDateFormat date = new SimpleDateFormat("yyMMddhhmmss");
 			String now = date.format(today);
-			String authornum = "00-"+now;
+			String authornum = "00-" + now;
 
 			// Add Author into DB
 			query = "INSERT INTO authors ";
@@ -178,39 +174,40 @@ public class BooksDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-        
-    	return true;
-    }
-	
+
+		return true;
+	}
+
 	// 도서 삭제
-	public void deleteBooks(JoinTable j){
+	public void deleteBooks(JoinTable j) {
 		try {
 			con = dataFactory.getConnection();
-			// TODO: JoinTable 에서 Booknum, authornum 도출
+			// JoinTable 에서 Booknum, authornum 도출
 			String booknum = j.getBooknum();
-			String authornum = j.getAuthornum();
-			// TODO: authornum 분리 작업 필요
-			// TODO: Delete 수행
-			// piece delete
-			System.out.println(booknum);
-			String query = "DELETE FROM pieces WHERE booknum = '" + booknum + "'AND authornum = '" + authornum + "'";
-			pstmt = con.prepareStatement(query);
-			pstmt.executeUpdate();
+			// authornum 분리 작업 필요
+			String[] authornum = j.getAuthornum().split(",");
 
+			// Delete 수행
+			String query;
+			for (String temp : authornum) {
+				// piece delete
+				query = "DELETE FROM pieces WHERE booknum = '" + booknum + "'AND authornum = '" + temp + "'";
+				pstmt = con.prepareStatement(query);
+				pstmt.executeUpdate();
+			}
 			// book delete
 			query = "DELETE FROM books WHERE booknum ='" + booknum + "'";
 			pstmt = con.prepareStatement(query);
 			pstmt.executeUpdate();
 			pstmt.close();
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-    public void createBooks(Books b, List<Pieces> l){
-   	    try {
+	public void createBooks(Books b, List<Pieces> l) {
+		try {
 			con = dataFactory.getConnection();
 
 			// TODO : book insert into DB
@@ -229,7 +226,7 @@ public class BooksDAO {
 			pstmt.executeUpdate();
 
 			// TODO : pieces insert into DB
-			for(Pieces p: l){
+			for (Pieces p : l) {
 				query = "INSERT INTO pieces";
 				query += " (booknum,authornum)";
 				query += " values(?,?)";
@@ -242,6 +239,6 @@ public class BooksDAO {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-                
-    }
+
+	}
 }
